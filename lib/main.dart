e90d3ff97337988c5ad5fb47f44d14ee25ee9c10
Gabriel Lucena <gabrielml371@gmail.com>
2021-02-10 11:50:31 -0300

    mobx applied

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:Bin2Dec/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-import 'home_page.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) => runApp(MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bin2Dec',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        scaffoldBackgroundColor: Colors.grey[800],
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
-      ),
-      home: HomePage(),
-    );
-  }
-}
+void main() => runApp(ModularApp(module: AppModule()));
